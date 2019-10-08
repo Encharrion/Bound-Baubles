4371d704583dbb64371e235ad6c826fb9aa7b607
Encharrion <haylward@gmail.com>
2019-10-08 01:17:48 -0700

    Added @Mod dependencies and moved mcmod.info again

--- src/main/java/encharrion/boundbaubles/BoundBaubles.java
+++ src/main/java/encharrion/boundbaubles/BoundBaubles.java
@@ -6,12 +6,13 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = BoundBaubles.MODID, name = BoundBaubles.NAME, version = BoundBaubles.VERSION)
+@Mod(modid = BoundBaubles.MODID, name = BoundBaubles.NAME, version = BoundBaubles.VERSION, dependencies = BoundBaubles.DEPENDENCIES)
 public class BoundBaubles
 {
     public static final String MODID = "boundbaubles";
     public static final String NAME = "Bound Baubles";
     public static final String VERSION = "0.1";
+    public static final String DEPENDENCIES = "required-after:baubles";
 
     public static Logger logger;
 
